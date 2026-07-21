40105dd6457f63de5e2c5d2099efb230158a46f6
Balram <balramsardar1111@gmail.com>
2026-07-21 22:18:04 +0530

    react project start

---
+++ Reactproject/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+  <div> <h1>heloo balram is here</h1></div>
+  );
+}
+
+export default App;
---
+++ viteproject/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+ 
+
+  return (
+  <h1> hello balram is here</h1>
+  )
+}
+
+export default App
