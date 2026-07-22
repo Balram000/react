89f6b8ebd2b4fdf056d30e866898e6d514ff2db3
Balram <balramsardar1111@gmail.com>
2026-07-22 22:46:28 +0530

    Implement counter functionality with add and remove buttons

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+let [counter ,setCounter] =useState(15)
+ // let counter = 15;
+  const addvalue = () => {
+    if(counter <20)
+    counter = counter + 1;
+    setCounter(counter)
+    console.log("add value ", counter);
+  }
+
+  const removevalue =( ) => {
+    if(counter>0){
+   setCounter(counter -1)
+   console.log(" remove value ", counter);
+    }
+   
+    
+  }
+
+  return (
+    <>
+      <h1>balram aur react </h1>
+      <h2>counter value {counter} </h2>
+
+      
+      <button onClick={addvalue}> add value </button>
+      <br />
+      <button onClick={removevalue}>remove value</button>
+    </>
+  )
+}
+
+export default App
