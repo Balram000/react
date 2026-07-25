372988c02b88bb25c9cf12e4250be932a1fe0249
Balram <balramsardar1111@gmail.com>
2026-07-25 12:26:10 +0530

    feat: initialize props project with Vite and React setup
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created favicon.svg and icons.svg for application branding.
    - Implemented main application structure in App.jsx with job listings.
    - Developed Card component to display job details.
    - Created User component for user-related functionality.
    - Added CSS styles for components to enhance UI.
    - Configured Vite for React application.

---
+++ props/src/component/Card.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Bookmark } from 'lucide-react'
+import './index.css';
+const Card = (props) => {
+  console.log(props);
+  return ( 
+    
+    
+    
+     <div className="card">
+        <div className="top">
+          <img src={props.logo} alt={props.company}  />
+          <button> Save <Bookmark size={12 } /></button>
+        </div>
+        <div className="center">
+          <h3>{props.company} <span> {props.posted} </span></h3>
+          <h2> {props.title}</h2>
+
+          <div className='tag'>
+            <h4> {props.type}</h4>
+            <h4> {props.level}</h4>
+          </div>
+        </div>
+        <div className="bottom">
+          <div>
+          <h3> {props.pay} </h3>
+          <h4>{props.location}</h4>
+          
+        </div>
+        <button> Apply now </button>
+        </div>
+       
+      </div>
+  )
+}
+
+export default Card
