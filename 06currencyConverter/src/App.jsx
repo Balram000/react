414c8332a56767d7f7e820ba0c362b3cc7b7a8ec
Balram <balramsardar1111@gmail.com>
2026-07-30 19:38:33 +0530

    Add initial setup for currency converter app with SVG assets, hooks, and Vite configuration
    - Added React and Vite SVG assets for React and Vite logos.
    - Created a custom hook `useCurrencyInfo` for fetching currency data.
    - Imported Tailwind CSS in the new `index.css` file.
    - Set up the main entry point in `main.jsx` to render the App component.
    - Configured Vite with React and Tailwind CSS plugins in `vite.config.js`.

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [count ,setCount]=useState(0)
+  return (
+    
+    <div>
+      <h1 className=' bg-orange-500 text-white text-center  text-4xl'>Currency</h1>
+    </div>
+  )
+}
+
+export default App
