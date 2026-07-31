8b4b8fce9a18b26c49b8224f0c3e244f06617407
Balram <balramsardar1111@gmail.com>
2026-07-31 21:24:48 +0530

    feat: add About, Contact, Footer, Github, Header, Home, and User components
    - Created About component with a description of React development.
    - Implemented Contact component with a form for user inquiries.
    - Added Footer component with navigation links and social media icons.
    - Developed Github component to display GitHub user information using API.
    - Created Header component for site navigation with links to Home, About, Contact, and Github.
    - Implemented Home component with promotional content and images.
    - Added User component to display user ID from URL parameters.
    - Set up routing in main.jsx for the new components.
    - Configured Vite for React and Tailwind CSS.
    - Added global styles in index.css for layout and design.

---
+++ 07reactrouter/src/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+import './App.css'
+
+
+const Layout = () => {
+  return (
+    <>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Layout
