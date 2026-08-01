e092cc24903092f1255b235e4b8a9a44f18a1023
Balram <balramsardar1111@gmail.com>
2026-08-01 13:28:59 +0530

    feat: add theme toggle button and card component, update styles and configurations

--- 09themeChanger/src/App.jsx
+++ 09themeChanger/src/App.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
 import { ThemeProvider } from './Contexts/Theme'
 import './App.css'
-
+import ThemeBtn from './components/ThemeBtn/ThemeBtn'
+import Card from './components/Card/Card'
 function App() {
   const [thememode, setThememode] = useState('light')
  const lightTheme=()=>{
@@ -18,18 +19,18 @@ document.querySelector('html').classList.add(thememode)
     
  },[thememode])
 
- 
+
   return (
     
    <ThemeProvider value={{thememode,lightTheme,darkTheme}}>
      <div className="flex flex-wrap min-h-screen items-center">
                     <div className="w-full">
                         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                            {/*themebtn*/}
+                            <ThemeBtn/>
                         </div>
     
                         <div className="w-full max-w-sm mx-auto">
-                           {/*card */}
+                           <Card/>
                         </div>
                     </div>
                 </div>
