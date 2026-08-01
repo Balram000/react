abc01ae544f0e71cf00b760c8f52fcc5b9731689
Balram <balramsardar1111@gmail.com>
2026-08-01 12:55:09 +0530

    feat: implement theme context and add theme toggle button and card component

--- 09themeChanger/src/App.jsx
+++ 09themeChanger/src/App.jsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useEffect, useState } from 'react'
+import { ThemeProvider } from './Contexts/Theme'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [thememode, setThememode] = useState('light')
+ const lightTheme=()=>{
+  setThememode('light')
+ }
+ const darkTheme =() =>{
+  setThememode('dark')
+ }
 
+ //actual  vhange intheme 
+ useEffect(()=>{
+  document.querySelector('html').classList.remove('light','dark')
+document.querySelector('html').classList.add(thememode)
+    
+ },[thememode])
+
+ 
   return (
     
-    <div className="flex flex-wrap min-h-screen items-center">
+   <ThemeProvider value={{thememode,lightTheme,darkTheme}}>
+     <div className="flex flex-wrap min-h-screen items-center">
                     <div className="w-full">
                         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
                             {/*themebtn*/}
@@ -20,6 +33,7 @@ function App() {
                         </div>
                     </div>
                 </div>
+   </ThemeProvider>
     
   )
 }
