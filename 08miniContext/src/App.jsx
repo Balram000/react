bc00287151c6aee50beb20abdc67a4272182f263
Balram <balramsardar1111@gmail.com>
2026-08-01 07:49:48 +0530

    feat: implement user authentication with context API and create Login and Profile components

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
+import Login from './component/Login/Login.jsx'
+import Profile from './component/Profile/Profile.jsx'
+import UserContextProvider from './context/UserContextProvider.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
- <div className='bg-blue-400 text-center '> Balram</div>
+    <UserContextProvider>
+      <h1>React with Balram </h1>
+      <Login />
+      
+      <Profile />
+    </UserContextProvider>
   )
 }
 
